cce78400992c66dafbb3011cc7c4969016401cff
Azirsec <43415156+Azirsec@users.noreply.github.com>
2019-11-08 21:38:01 -0500

    Added movement

--- A Gift You Could Not Give/Assets/Scripts/Player.cs
+++ A Gift You Could Not Give/Assets/Scripts/Player.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector3 direction = new Vector3(0, 0, 0);
     Vector3 velocity = new Vector3(0, 0, 0);
+    Vector3 acceleration = new Vector3(0, 0, 0);
+
+    public float friction;
+    public float speed;
+    public float maxSpeed;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +22,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        velocity.x = Input.GetAxis("Horizontal");
-        velocity.z = Input.GetAxis("Vertical");
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction = direction.normalized;
+        acceleration = direction * speed * Time.deltaTime;
+        velocity += acceleration;
+        velocity *= friction;
+
+        if (velocity.magnitude < maxSpeed)
+        {
+            gameObject.transform.position += velocity;
+        }
     }
 }
